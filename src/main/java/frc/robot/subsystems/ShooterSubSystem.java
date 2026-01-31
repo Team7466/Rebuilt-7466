bacdbbc1c87e6fc3986e6755a7b88b2d7ef56474
Yigit <yigit.olc@hotmail.com>
2026-01-31 02:44:14 +0000

    shooter eklendi

--- src/main/java/frc/robot/subsystems/ShooterSubSystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubSystem.java
@@ -1,1 +1,59 @@
+package frc.robot.subsystems;
 
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ShooterConstants;
+
+public class ShooterSubsystem extends SubsystemBase {
+  private final SparkMax shooterMotor;
+  private SparkMaxConfig motorConfig;
+
+  private DigitalInput beamBreak;
+  private AnalogInput infraRed;
+  Debouncer debounce;
+
+  public ShooterSubsystem() {
+    shooterMotor = new SparkMax(ShooterConstants.shooterMotor, MotorType.kBrushless);
+    motorConfig = new SparkMaxConfig();
+    setConfigs();
+    applyConfigs();
+  }
+
+  /** Set parameters for the SPARK. */
+  private void setConfigs() {
+    motorConfig
+        .smartCurrentLimit(50)
+        .idleMode(IdleMode.kBrake)
+        .openLoopRampRate(0.15)
+        .voltageCompensation(12.0);
+
+    motorConfig
+        .signals
+        .appliedOutputPeriodMs(10)
+        .primaryEncoderPositionPeriodMs(500)
+        .primaryEncoderVelocityPeriodMs(20);
+  }
+
+  private void applyConfigs() {
+    shooterMotor.configure(
+        motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
+
+  public void shooterSetSpeed(double speed) {
+
+    shooterMotor.set(speed);
+  }
+
+  public void shooterStop() {
+
+    shooterMotor.set(0.0);
+  }
+}
