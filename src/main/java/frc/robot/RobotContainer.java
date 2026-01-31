fd5e9d4907a1190753944f6e53064a92e039cb2d
Yigit <yigit.olc@hotmail.com>
2026-01-31 11:46:06 +0000

    buton ekle

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -71,12 +71,12 @@ public class RobotContainer {
     driverPS.R1().whileTrue(Commands.run(() -> speed = 0.6));
 
     driverPS.R1().onFalse(Commands.run(() -> speed = 1.0));
-    driverPS
-        .L1()
-        .whileTrue(m_IntakeSubSystem.run(() -> m_IntakeSubSystem.intakeSetSpeed(0.6))); // motor
+    driverPS.L1().whileTrue(m_IntakeSubSystem.run(() -> m_IntakeSubSystem.intakeSetSpeed(0.6)));
+    driverPS.L1().whileTrue(m_ShooterSubsystem.run(() -> m_ShooterSubsystem.shooterSetSpeed(0.7))); // motor
 
-    driverPS.L2().whileTrue(m_ShooterSubsystem.run(() -> m_ShooterSubsystem.shooterSetSpeed(0.7)));
-    driverPS.R2().whileTrue(m_ShooterSubsystem.run(() -> m_ShooterSubsystem.shooterSetSpeed(-0.7)));
+    driverPS.L2().whileTrue(m_ShooterSubsystem.run(() -> m_ShooterSubsystem.shooterSetSpeed(-1.0)));
+    driverPS.L2().whileTrue(m_IntakeSubSystem.run(() -> m_IntakeSubSystem.intakeSetSpeed(1.0)));
+
 
 
 
